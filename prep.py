f572b67b55a3fd137a6a94fbbe5c0d2a77db1027
HirokiMatsuzaki <nobunaga.no.yabou.sophia@gmail.com>
2018-08-28 20:10:05 +0900

    preprocessorの実装

---
+++ prep.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+def preprocessor(text):
+  text = re.sub('<[^>]*>', '', text)
+  emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+  text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+  return text
